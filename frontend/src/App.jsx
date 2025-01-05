f38aee72832a82665d32732564e218e6d197f9ad
PKR9759 <kuldiprparmar9759@gmail.com>
2025-01-05 16:10:41 +0530

    feat:complete Authentication with login and register functinality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from '../src/features/Auth/pages/Login';
+import Register from '../src/features/Auth/pages/Register';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-     
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
